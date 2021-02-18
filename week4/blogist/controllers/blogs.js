1127834dcdd380962d0aa34daa887dfa63cd1eee
Lauri Karanko <lauri.karanko@gmail.com>
2021-02-18 23:36:58 +0200

    week4 exercises 1,2

---
+++ week4/blogist/controllers/blogs.js
@@ -0,0 +1,29 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    console.log("getti")
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+    console.log("posti")
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
+
+// const PORT = 3003
+
+// blogsRouter.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+// })
+
+module.exports = blogsRouter

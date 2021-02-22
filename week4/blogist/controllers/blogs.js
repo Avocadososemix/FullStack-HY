3fe34f381a6b3f705b09193e7b5664491f8bf0fe
Lauri Karanko <lauri.karanko@gmail.com>
2021-02-22 20:47:19 +0200

    week4 exercises 1-13 done

--- week4/blogist/controllers/blogs.js
+++ week4/blogist/controllers/blogs.js
@@ -10,20 +10,77 @@ blogsRouter.get('/', (request, response) => {
         })
 })
 
-blogsRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-    console.log('posti')
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+// blogsRouter.post('/', (request, response) => {
+//     const blog = new Blog(request.body)
+//     console.log('posti')
+//     blog
+//         .save()
+//         .then(result => {
+//             response.status(201).json(result)
+//         })
+// })
+
+// blogsRouter.post('/', async (request, response, next) => {
+//     const body = request.body
+
+//     const blog = new Blog({
+//         title: body.content,
+//         author: body.author === undefined ? 'unknown' : body.author,
+//         url: body.url === undefined ? 'unknown' : body.url,
+//         likes: body.likes === undefined ? 0 : body.likes,
+//     })
+//     try {
+//         const savedBlog = await blog.save()
+//         response.json(savedBlog.toJSON())
+//     } catch(exception) {
+//         next(exception)
+//     }
+// })
+
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author === undefined ? 'unknown' : body.author,
+        url: body.url,
+        likes: body.likes === undefined ? 0 : body.likes,
+    })
+
+    if (blog.title === undefined || blog.url === undefined) {
+        response.status(400).end()
+    } else {
+        const savedBlog = await blog.save()
+        response.json(savedBlog.toJSON())
+    }
 })
 
-// const PORT = 3003
+blogsRouter.get('/:id', async (request, response) => {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+        response.json(blog.toJSON())
+    } else {
+        response.status(404).end()
+    }
+})
 
-// blogsRouter.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`)
+// blogsRouter.put('/:id', (request, response) => {
+//     const body = request.body
+
+//     const blog = new Blog({
+//         title: body.title,
+//         author: body.author === undefined ? 'unknown' : body.author,
+//         url: body.url,
+//         likes: body.likes === undefined ? 0 : body.likes,
+//     })
+
+//     const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+//     response.json(updatedBlog)
 // })
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+})
 
 module.exports = blogsRouter
---
+++ week4/blogist/tests/blog_api.test.js
@@ -0,0 +1,181 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const helper = require('./test_helper')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('4.8 a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.title)
+
+    expect(contents).toContain(
+        'React patterns'
+    )
+})
+
+test('4.9 blogs are identified with an', async () => {
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.title)
+
+    expect(contents).toContain(
+        'React patterns'
+    )
+})
+
+test('4.10 a valid blog can be added ', async () => {
+    const newBlog = {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const titles = blogsAtEnd.map(n => n.title)
+    expect(titles).toContain(
+        'React patterns'
+    )
+})
+
+test('blog without title is not added', async () => {
+    const newBlog = {
+        author: 'Mister Cake'
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+})
+
+test('3.9 a specific blog can be viewed using the id', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+
+    const blogToView = blogsAtStart[0]
+    expect(blogToView.id).toBeDefined()
+
+    const resultBlog = await api
+        .get(`/api/blogs/${blogToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const processedBlogToView = JSON.parse(JSON.stringify(blogToView))
+
+    expect(resultBlog.body.id).toEqual(blogToView.id)
+    expect(resultBlog.body).toEqual(processedBlogToView)
+})
+
+test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(
+        helper.initialBlogs.length - 1
+    )
+
+    const titles = blogsAtEnd.map(r => r.title)
+
+    expect(titles).not.toContain(blogToDelete.title)
+})
+
+test('4.11 blog without likes receives 0 likes', async () => {
+    const newBlog = {
+        title: 'React magic patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const title = blogsAtEnd.map(n => n.title)
+    expect(title).toContain(
+        'React magic patterns'
+    )
+
+    const likes = blogsAtEnd.map(n => n.likes)
+    expect(likes).toContain(
+        0
+    )
+})
+
+test('4.12 adding blog without title or url fails', async () => {
+    const newBlog = [{
+        author: 'Michael Chan',
+        likes: 3,
+    }, {
+        title: 'Some title',
+        author: 'Michael Chan',
+        likes: 3,
+    }, {
+        author: 'Michael Chan',
+        url: 'www.testurl.com',
+        likes: 3,
+    }
+    ]
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog[0])
+        .expect(400)
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog[1])
+        .expect(400)
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog[2])
+        .expect(400)
+
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
